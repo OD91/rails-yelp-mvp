d17071022c564bc91d3ce64180629b8742c5458d
ole dening <ole.dening@gmail.com>
2018-11-14 16:51:58 +0100

    yelp MVP done

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,40 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts "Destroying all records..."
+
+Restaurant.destroy_all
+
+puts "adding new restaurants..."
+
+restaurants_attributes = [
+  {
+    name:         'Dishoom',
+    address:      '7 Boundary St, London E2 7JE',
+    category:  'chinese'
+  },
+  {
+    name:         'Pizza East',
+    address:      '56A Shoreditch High St, London E1 6PQ',
+    category:  'french'
+  },
+  {
+    name: "Tim Raue",
+    address: "Berlin",
+    category: "belgian"
+  },
+  {
+    name: "Charlotte",
+    address: "Checkpoint Charlie",
+    category: "italian"
+  },
+  {
+    name: "Xin",
+    address: "Checkpoint Charlie",
+    category: "japanese"
+  }
+]
+
+Restaurant.create!(restaurants_attributes)
+
+puts "Restaurants added"
